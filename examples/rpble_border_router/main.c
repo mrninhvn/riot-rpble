d07deb0797e2b09993343212cf83c044186355ec
mrninhvn <mr.ninnh.vn@outlook.com>
2025-03-25 16:09:16 +0700

    rpble examplke running

--- examples/rpble_border_router/main.c
+++ examples/rpble_border_router/main.c
@@ -22,6 +22,7 @@
 
 #include "shell.h"
 #include "msg.h"
+#include "net/gnrc/rpl.h"
 
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
@@ -37,6 +38,14 @@ int main(void)
     puts("All up, running the shell now");
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    gnrc_netif_t *netif = gnrc_netif_get_by_pid(10);
+    const ipv6_addr_t dodang = { {
+                                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                                0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
+        }
+    };
+    gnrc_rpl_configure_root(netif, &dodang);
 
     /* should be never reached */
     return 0;
