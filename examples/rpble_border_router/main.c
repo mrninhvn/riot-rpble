5ccfcde4d39a390b11017421f806fc093487e222
mrninhvn <mr.ninnh.vn@outlook.com>
2025-04-08 10:44:23 +0700

    auto init rpl esp32s3

--- examples/rpble_border_router/main.c
+++ examples/rpble_border_router/main.c
@@ -39,14 +39,6 @@ int main(void)
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
-    gnrc_netif_t *netif = gnrc_netif_get_by_pid(10);
-    const ipv6_addr_t dodang = { {
-                                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-                                0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
-        }
-    };
-    gnrc_rpl_configure_root(netif, &dodang);
-
     /* should be never reached */
     return 0;
 }
